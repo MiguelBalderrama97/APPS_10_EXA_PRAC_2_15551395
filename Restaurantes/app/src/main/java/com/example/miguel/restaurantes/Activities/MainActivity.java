66ece4bdf69ec7651251522679f81b5402e744e0
MartinCJ08 <martin.carrascoj@gmail.com>
2019-04-06 11:13:05 -0700

    Captura funcional

--- Restaurantes/app/src/main/java/com/example/miguel/restaurantes/Activities/MainActivity.java
+++ Restaurantes/app/src/main/java/com/example/miguel/restaurantes/Activities/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.miguel.restaurantes.Activities;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.miguel.restaurantes.Models.Restaurante;
 import com.example.miguel.restaurantes.R;
 
 public class MainActivity extends AppCompatActivity {
     Intent inDatos;
+    Restaurante rDato;
+    final int RESTARUANTE_REQUEST=420;
 
 
     @Override
@@ -22,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View v){
         switch (v.getId()){
             case R.id.btnCap:
-                startActivity(inDatos);
+                startActivityForResult(inDatos,RESTARUANTE_REQUEST);
                 break;
             case R.id.btnMostrar:
                 startActivity(inDatos);
@@ -33,5 +39,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+
+        if(requestCode == RESTARUANTE_REQUEST){
+            if(resultCode == Activity.RESULT_OK){
+                int icon =data.getIntExtra("icon",R.drawable.barrafina);
+                String nom = data.getStringExtra("nom");
+                String desc = data.getStringExtra("desc");
+                String dir=data.getStringExtra("dir");
+                rDato = new Restaurante(nom,desc,dir,icon,1); //Este es el mero dato
+
+            }else if(resultCode == Activity.RESULT_CANCELED){
+                Toast.makeText(getApplicationContext(),"Cancelado",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
